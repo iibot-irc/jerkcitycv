fd86aaedecbe83d77f63deeb8c18f674eb929a33
Jacob Parker <j@cob.xxx>
2014-01-15 05:57:44 -0500

    Made tests go in parallel; some more notes

--- tests/all.sh
+++ tests/all.sh
@@ -1,27 +1,10 @@
 #!/bin/bash
+
+MAX=5464
+PARALLEL=80
+
 rm -rf out
 mkdir out
-for NUM in `seq 1 5464`; do
-  echo $NUM
-  OUT=`./test.sh $NUM`
-  CODE=$?
-  echo '<!doctype html><meta charset="utf-8"><h1>Test results for comic #'$NUM'</h1><img src="http://jerkcity.com/jerkcity'$NUM'.gif">'"$OUT" > out/$NUM.html
-  case $CODE in
-    1*)
-      COLOR="grey"
-      ;;
-    2*)
-      COLOR="yellow"
-      ;;
-    3*)
-      COLOR="red"
-      ;;
-    4*)
-      COLOR="purple"
-      ;;
-    0)
-      COLOR="green"
-      ;;
-  esac
-  echo '<a href="'$NUM'" style="font-size: 20; background: '$COLOR'">'$NUM'</a>' >> out/index.html
-done
+seq 1 $MAX | xargs -n 1 -P $PARALLEL ./test.sh
+seq -f "out/%g.dat" 1 $MAX | xargs cat > out/index.html
+rm out/*.dat
--- tests/test.sh
+++ tests/test.sh
@@ -2,6 +2,8 @@
 NUM=$1
 EXPECTED=`awk "/^JERKCITY \#$NUM:/{a=1;next}/--cut here--/{a=0}a" jerkcity.txt | tail -n +3`
 
+echo "<img src=\"http://jerkcity.com/jerkcity$NUM.gif\">" > out/$NUM.html
+
 if [ ! -f $NUM.png ]; then
   wget --quiet http://jerkcity.com/jerkcity$NUM.gif
   convert jerkcity$NUM.gif $NUM.png
@@ -11,44 +13,46 @@ fi
 cd ../
 ACTUAL=`./jerkcity tests/$NUM.png 2>/dev/null | sed -e 's/ *$//' -e 's/^ *//'`
 EX=$?
+cd tests
 
 if [[ $EX -ne 0 ]]; then
-  echo "<p>Non-zero exit code: $EX</p>"
-  exit 40
+  echo "<a href=\"$NUM.html\" style=\"font-size: 20; display: block-inline; width: 5em; background: purple\">$NUM</a>" > out/$NUM.dat
+  echo "<p>Non-zero exit code: $EX</p>" >> out/$NUM.html
+  exit
 fi
 
 EXESC=`echo "$EXPECTED" | sed -e 's/</\&lt;/g' -e 's/>/\&gt;/g'`
 ACESC=`echo "$ACTUAL" | sed -e 's/</\&lt;/g' -e 's/>/\&gt;/g'`
 
 if [[ $EXPECTED == "" ]]; then
-  echo "<p>No expected data.<p>"
-  echo "<h2>Actual</h2><pre>$ACESC</pre>"
-  exit 10
+  echo "<a href=\"$NUM.html\" style=\"font-size: 20; display: block-inline; width: 5em; background: grey\">$NUM</a>" > out/$NUM.dat
+  echo "<p>No expected data.<p><h2>Actual</h2><pre>$ACESC</pre>" >> out/$NUM.html
+  exit
 fi
 
 EXLINES=`echo "$EXPECTED" | wc -l`
 ACLINES=`echo "$ACTUAL" | wc -l`
 
 if [[ $EXLINES -ne $ACLINES ]]; then
-  echo "<p>Line count mismatch.</p>"
-  echo "<h2>Expected</h2><pre>$EXESC</pre><h2>Actual</h2><pre>$ACESC</pre>"
-  exit 30
+  echo "<a href=\"$NUM.html\" style=\"font-size: 20; display: block-inline; width: 5em; background: red\">$NUM</a>" > out/$NUM.dat
+  echo "<p>Line count mismatch.</p><h2>Expected</h2><pre>$EXESC</pre><h2>Actual</h2><pre>$ACESC</pre>" >> out/$NUM.html
+  exit
 fi
 
 EXWORDS=`echo "$EXPECTED" | sed -e 's/^[^:]*: //'`
 ACWORDS=`echo "$ACTUAL" | sed -e 's/^[^:]*: //'`
 
 if [[ "$EXWORDS" != "$ACWORDS" ]]; then
-  echo "<p>Incorrect dialog.</p>"
-  echo "<h2>Expected</h2><pre>$EXESC</pre><h2>Actual</h2><pre>$ACESC</pre>"
-  exit 31
+  echo "<a href=\"$NUM.html\" style=\"font-size: 20; display: block-inline; width: 5em; background: red\">$NUM</a>" > out/$NUM.dat
+  echo "<p>Incorrect dialog.</p><h2>Expected</h2><pre>$EXESC</pre><h2>Actual</h2><pre>$ACESC</pre>" >> out/$NUM.html
+  exit
 fi
 
 if [[ "$EXPECTED" != "$ACTUAL" ]]; then
-  echo "<p>Incorrect cast.</p>"
-  echo "<h2>Expected</h2><pre>$EXESC</pre><h2>Actual</h2><pre>$ACESC</pre>"
-  exit 20
+  echo "<a href=\"$NUM.html\" style=\"font-size: 20; display: block-inline; width: 5em; background: yellow\">$NUM</a>" > out/$NUM.dat
+  echo "<p>Incorrect cast.</p><h2>Expected</h2><pre>$EXESC</pre><h2>Actual</h2><pre>$ACESC</pre>" >> out/$NUN.html
+  exit
 fi
 
-echo "<p>Yay!</p>"
-echo "<h2>Expected</h2><pre>$EXESC</pre><h2>Actual</h2><pre>$ACESC</pre>"
+echo "<a href=\"$NUM.html\" style=\"font-size: 20; display: block-inline; width: 5em; background: green\">$NUM</a>" > out/$NUM.dat
+echo "<p>Yay!</p><h2>Expected</h2><pre>$EXESC</pre><h2>Actual</h2><pre>$ACESC</pre>" >> out/$NUM.html
